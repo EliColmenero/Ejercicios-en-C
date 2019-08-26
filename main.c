9592f322188bcebd4c24f65881b3bcc9ca83622e
EliColmenero <52494848+EliColmenero@users.noreply.github.com>
2019-08-26 12:25:28 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,62 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int sumar1(int a, int b); //Prototipo de la funcion
+
 int main()
 {
-    char opcion;
-    char seguir;
+    int numero1;
+    int numero2;
+    int suma;
 
-    do{
+    printf("Ingrese un numero: ");
+    scanf("%d", &numero1);
 
-        system("cls");
-        printf("--Menu de opcions--\n\n\n");
-        printf("a- Sumar\n");
-        printf("b- Restar\n");
-        printf("c- Multiplicacion");
-        printf("d- Dividir\n");
-        printf("f- Salir\n\n");
-        printf("Ingrese la opcion: ");
-        opcion = getche();
+    printf("Ingrese otro numero: ");
+    scanf("%d", &numero2);
 
-        switch(opcion){
-
-        case 'a':
-            printf("\nUsted eligio la opcion sumar \n");
-            system("pause"); //Para el programa y espera a que apretemos enter u otra tecla para continuar
-            //getch; para linux
-            break;
-
-        case 'b':
-            printf("\nUsted eligio la opcion restar \n");
-            system("pause");
-            break;
-
-        case 'c':
-            printf("\nUsted eligio la opcion multiplicar \n");
-            system("pause");
-            break;
-
-        case 'd':
-            printf("\nUsted eligio la opcion dividir \n");
-            system("pause");
-            break;
-
-        case 'e':
-            printf("\nUsted eligio la opcion salir \n");
-            printf("Confirma la salida?");
-            seguir = getche();
-            system("pause");
-            break;
-
-        default:
-            printf("\nOpcion invalida \n\n");
-            system("pause");
-            break; //codeblocks obliga a poner break en default
+    suma = sumar1(numero1, numero2);
 
 
-        }
+    printf("La suma es ", suma);
 
-    }while(seguir=='n');
+
 
     return 0;
+}
+int sumar1(int a, int b){
+    int suma;
+    suma = a + b;
+    return suma;
 }
