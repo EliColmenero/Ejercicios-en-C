29fbc93844328c435cd822ac6dbde6fda75ae410
EliColmenero <52494848+EliColmenero@users.noreply.github.com>
2019-08-28 10:31:52 -0300

    Tipos de funciones
    Funcione que devuelve y recibe

--- main.c
+++ main.c
@@ -1,32 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-int sumar1(int a, int b); //Prototipo de la funcion
+int sumarV1(int a, int b);
 
 int main()
 {
-    int numero1;
-    int numero2;
-    int suma;
+    int resultado;
+    int num1;
+    int num2;
 
-    printf("Ingrese un numero: ");
-    scanf("%d", &numero1);
+     printf("Ingrese un numero: ");
+    scanf("%d", &num1);
 
     printf("Ingrese otro numero: ");
-    scanf("%d", &numero2);
+    scanf("%d", &num2);
 
-    suma = sumar1(numero1, numero2);
+   resultado =  sumarV1(num1, num2);
 
-
-    printf("La suma es ", suma);
-
-
+   printf("La suma es %d\n\n", resultado);
 
     return 0;
 }
-int sumar1(int a, int b){
-    int suma;
-    suma = a + b;
-    return suma;
+
+int sumarV1(int num1, int num2){
+
+    int resultado;
+
+    resultado = num1 + num2;
+
+    return resultado;
+
 }
